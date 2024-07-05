cef517383c6dbbd34375d887b968737240b77664
Aspasija <aspasija.cvetkoska@result.si>
2024-07-05 21:54:36 +0200

    added image in container two in homepage

--- src/Components/HomePageComponents/ContainerTwo.jsx
+++ src/Components/HomePageComponents/ContainerTwo.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import SportsCar from "../../Images/white-coupe-sport.jpg";
 
 const ContainerTwo = () => {
   return (
-    <div>ContainerTwo</div>
+    <div style={{ width: "650px", overflow: "hidden", borderRadius: "10px" }}>
+      <img
+        style={{ width: "100%", height: "auto", objectFit: "cover", borderRadius: "10px" }}
+        src={SportsCar}
+        alt="sport-car-container"
+      />
+    </div>
   )
 }
 
-export default ContainerTwo
+export default ContainerTwo;
